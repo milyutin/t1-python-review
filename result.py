bc6b1dcac5b447639e2c154e5bd63f5a3cdef261
milyutin <in.milyutin@gmail.com>
2025-07-23 16:58:27 +0300

    Create result.py

---
+++ result.py
@@ -0,0 +1,17 @@
+def res():
+    data = [
+        {"name": "Hotel A", "stars": 4, "rooms": [{"capacity": 2}, {"capacity": 3}]},
+        {"name": "Hostel B", "stars": 2, "rooms": [{"capacity": 1}, {"capacity": 1}, {"capacity": 2}]},
+        {"name": "Apart C", "stars": 3, "rooms": []},
+        {"name": "Hotel D", "stars": 5, "rooms": [{"capacity": 4}]}
+    ]
+
+    result = {
+        hotel["name"]: sum(
+            room["capacity"]
+            for room in hotel["rooms"]
+            if room["capacity"] > 1
+        )
+        for hotel in data
+        if hotel["stars"] >= 3 and hotel["rooms"]
+    }
